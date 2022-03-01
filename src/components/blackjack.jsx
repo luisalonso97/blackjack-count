07d1d2a341bf03c683799d14bcd5ad5a9b75dbc9
Luis Alonso <luisalonso97@gmail.com>
2022-02-28 21:21:05 -0600

    Updated game cointainer color

--- src/components/blackjack.jsx
+++ src/components/blackjack.jsx
@@ -1,7 +1,7 @@
 
 export const Blackjack = () => {
   return (
-    <game className="bg-orange-500 flex flex-row h-full w-full md:flex-col">
+    <game className="bg-p-beige-light flex flex-row h-full w-full md:flex-col">
       {/* <div className="bg-orange-500 shadow-xl h-16 bottom-0 text-white md:relative md:h-[calc(100vh_-_3.5rem)] z-10 w-full md:w-96 content-center">
         <div className="md:fixed content-center md:content-start text-left justify-between md:w-60">
           <ul className="list-reset grid grid-flow-col md:grid-flow-row pt-1 px-1 md:px-2 text-center md:text-left items-center">
